d7c6e10b00d42517069707554110ed1836cf38f0
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-08 17:55:34 +0200

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import List from './List';
 
 const App = () => {
   const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button className="btn btn-block" onClick={() => setPeople([])}>
+          clear all
+        </button>
       </section>
     </main>
   );
--- src/Person.jsx
+++ src/Person.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-
-const Person = () => {
-  return <div>Person</div>;
+const Person = ({ image, name, age }) => {
+  return (
+    <article className="person">
+      <img className="img" src={image} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
 };
 
 export default Person;
