f15f29ff77ad88b3e1dd3b68fa9a56a8e31c89c7
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-08 17:45:20 +0200

    create new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
+  const [people, setPeople] = useState(data);
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
